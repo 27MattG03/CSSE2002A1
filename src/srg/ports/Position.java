6c95f2a35a7020d29b72d7329fc808d90ccff60c
MATTHEW.GREENWOOD <s4699133L@student.uq.edu.au>
2023-04-06 15:55:04 +1000

    Tests changed to JUnit4

--- src/srg/ports/Position.java
+++ src/srg/ports/Position.java
@@ -3,39 +3,50 @@ package srg.ports;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
+/**
+ * Position class.
+ */
 public class Position extends Object {
+    /**
+     * x coordinate
+     */
     public final int x;
+    /**
+     * y coordinate
+     */
     public final int y;
+    /**
+     * x coordinate
+     */
     public final int z;
 
     /**
-     *
-     * @param x
-     * @param y
-     * @param z
+     * Constructs a position object.
+     * @param x x-coord
+     * @param y y-coord
+     * @param z z-coord
      */
     public Position(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-
     /**
-     *
+     * Gets the distance between 2 position object.
      * @param other The position that the distance will be calculated to.
      * @return Distance to the other position.
      */
-    public int distanceTo(Position other){
-        int dist = (int) sqrt(pow((other.x-this.x),2)+pow((other.y-this.y),2)+pow((other.z-this.z),2));
+    public int distanceTo(Position other) {
+        int dist = (int) sqrt(pow((other.x - this.x), 2) + pow((other.y - this.y),2) + pow((other.z - this.z), 2));
         return dist;
     }
 
     /**
-     *
+     * Gets a String representation of the pos object
      * @return A string representation of a position object.
      */
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("(%d, %d, %d)", x, y, z);
     }
 }
