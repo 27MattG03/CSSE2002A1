0814d02274d4db9e2bccb1d240159455d993c99c
MATTHEW.GREENWOOD <s4699133L@student.uq.edu.au>
2023-04-06 11:19:22 +1000

    Position fixed

--- src/srg/resources/ResourceContainer.java
+++ src/srg/resources/ResourceContainer.java
@@ -1,7 +1,7 @@
 package srg.resources;
 
 public class ResourceContainer extends Object{
-    static final int MAXIMUM_CAPACITY = 10;
+    public static final int MAXIMUM_CAPACITY = 10;
     private int amount;
     private ResourceType type;
     public ResourceContainer(ResourceType type, int amount) {
