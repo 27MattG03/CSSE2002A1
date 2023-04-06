5fcabd8a1b0d72680e168e942bd38c9e8b9cf3e6
MATTHEW.GREENWOOD <s4699133L@student.uq.edu.au>
2023-04-06 15:04:12 +1000

    Tests changed to JUnit4

--- test/srg/ports/PositionTest.java
+++ test/srg/ports/PositionTest.java
@@ -1,8 +1,10 @@
 package srg.ports;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.Assert.*;
 
 class PositionTest {
 
